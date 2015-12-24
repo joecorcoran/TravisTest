cf4fffd9632a5f3d62c6b0d0d08246073d23bb0b
Michael Updegraff <michael.updegraff@gmail.com>
2015-12-24 08:53:01 -0800

    Added testAsynchronousURLConnection XCTest

--- TravisTestTests/TravisTestTests.swift
+++ TravisTestTests/TravisTestTests.swift
@@ -11,26 +11,55 @@ import XCTest
 
 class TravisTestTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  let dict: [String: AnyObject] = ["title": "Title Test", "eyebrow": "Eyebrow Test", "date": ""]
+  
+  var event = Event?()
+  
+  func testEventValues() {
+    
+    event = Event(dictionary: dict)
+    
+    let name = event!.name
+    let expectedName = "Title Test"
+    XCTAssertEqual(expectedName, name!, "the value should be Title Test")
+    
+    let eyebrow = event!.eyebrow
+    let expectedEyebrow = "Eyebrow Test"
+    XCTAssertEqual(expectedEyebrow, eyebrow!, "the value should be Title Test")
+    
+  }
+  
+  func testAsynchronousURLConnection() {
+    let URL = NSURL(string: "https://upworks.io/api/events/event_listing.events.json")!
+    let expectation = expectationWithDescription("GET \(URL)")
+    
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithURL(URL) { data, response, error in
+      XCTAssertNotNil(data, "data should not be nil")
+      XCTAssertNil(error, "error should be nil")
+      
+      if let HTTPResponse = response as? NSHTTPURLResponse,
+        responseURL = HTTPResponse.URL,
+        MIMEType = HTTPResponse.MIMEType
+      {
+        XCTAssertEqual(responseURL.absoluteString, URL.absoluteString, "HTTP response URL should be equal to original URL")
+        XCTAssertEqual(HTTPResponse.statusCode, 200, "HTTP response status code should be 200")
+        XCTAssertEqual(MIMEType, "application/json", "HTTP response content type should be application/json")
+      } else {
+        XCTFail("Response was not NSHTTPURLResponse")
+      }
+      
+      expectation.fulfill()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    task.resume()
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    waitForExpectationsWithTimeout(task.originalRequest!.timeoutInterval) { error in
+      if let error = error {
+        print("Error: \(error.localizedDescription)")
+      }
+      task.cancel()
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
