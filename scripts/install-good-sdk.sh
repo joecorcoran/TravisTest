e4f931c099a8923916d01f86dac875a46d374610
Michael Updegraff <michael.updegraff@gmail.com>
2016-02-28 12:08:32 -0800

    Script changes for GoodSDK - moved commands into .travis.yml

--- scripts/install-good-sdk.sh
+++ scripts/install-good-sdk.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # copy good sdk zip to /Applications/Xcode.app/Contents/Developer
-sudo cp ./Good_Dynamics_client_SDK_for_iOS_v2.1.4439.tar.gz /Applications/Xcode.app/Contents/Developer
+cp ./scripts/Good_Dynamics_client_SDK_for_iOS_v2.1.4439.tar.gz /Applications/Xcode.app/Contents/Developer
 # cd to /Applications/Xcode.app/Contents/Developer
 cd /Applications/Xcode.app/Contents/Developer
 # unzip Good_Dynamics_client_SDK_for_iOS_v2.1.4439.tar.gz
-sudo tar zxvf Good_Dynamics_client_SDK_for_iOS_v2.1.4439.tar.gz
+tar zxvf Good_Dynamics_client_SDK_for_iOS_v2.1.4439.tar.gz
