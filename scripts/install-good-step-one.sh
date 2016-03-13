a13a1ccae124bd3da7bd0876ef34c66fcac408b4
Michael Updegraff <michael.updegraff@gmail.com>
2016-03-13 16:48:20 -0700

    Testing chnanges

--- scripts/install-good-step-one.sh
+++ scripts/install-good-step-one.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
-pwd
+cd /Applications/Xcode.app/Contents/Developer
+echo "$GOOD_ZIP"
+
 for file in "$GOOD_ZIP"
 do
     tar -xvf "$file"
 done
+
+cd ./Good.Platform
+
+mkdir "$FIPS_DIR"
+
+cp "$FIPS_FILE" ./"$FIPS_DIR"
+
+cd ./"$FIPS_DIR"
+
+for file2 in $FIPS_FILE
+do
+  tar -xvf "$file2"
+done
+
+rm "$FIPS_FILE"
+cd ../
+rm "$FIPS_FILE"
