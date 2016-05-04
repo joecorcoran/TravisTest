907d06573da14223d60fec04d62237ef9200d89a
Joe Corcoran <joe@corcoran.io>
2016-05-04 14:47:43 +0200

    Test

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -17,7 +17,10 @@ echo "*        Signing          *"
 echo "***************************"
 xcrun -log -v -sdk iphoneos9.2 PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
- sudo zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" " $OUTPUTDIR/$APP_NAME.app"
+echo $OUTPUTDIR
+echo $APP_NAME
+ls -la $OUTPUTDIR
+sudo zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" " $OUTPUTDIR/$APP_NAME.app"
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
